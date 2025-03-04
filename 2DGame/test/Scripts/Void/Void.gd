8e563ea2a1f871af54273d5dba582c4de096ee70
TrollingboygamingYT <147536768+trollingboygamingYT@users.noreply.github.com>
2025-03-04 10:57:06 +0100

    outlining Overworld

--- 2DGame/test/Scripts/Void/Void.gd
+++ 2DGame/test/Scripts/Void/Void.gd
@@ -5,4 +5,5 @@ func _ready():
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		get_tree().quit()
+		queue_free()
+		get_tree().change_scene_to_file("res://Scenes/RespawnScreen.tscn")
