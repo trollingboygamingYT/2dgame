dc83631d0c652e6373a81f065dc1cc5c6ec7fb82
Eliaspro10 <154473909+Eliaspro10@users.noreply.github.com>
2025-03-08 23:59:04 +0100

    Player Movement Data P3

--- 2DGame/test/Scripts/Player/Player.gd
+++ 2DGame/test/Scripts/Player/Player.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 600.0
-const ACCELERATION = 600.0
-const FRICTION = 1000.0
-const JUMP_VELOCITY = -550.0
+@export var movement_data : PlayerMovementData
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var coyote_jump_timer: Timer = $CoyoteJumpTimer
 
 func _physics_process(delta):
 	apply_gravity(delta)
@@ -13,24 +12,37 @@ func _physics_process(delta):
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
+	apply_air_resistance(input_axis, delta)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
+		
+	#Switch movement in realtime (Key = Backspace) (Can be power up)
+	if Input.is_action_just_pressed("Switch_movement"):
+		movement_data = load("res://Prefabs/FastMovementData.tres")
 
 func apply_gravity(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * movement_data.grabity_scale * delta
 
 func handle_jump():
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_accept"):
-			velocity.y = JUMP_VELOCITY
-	else:
-		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
-			velocity.y = JUMP_VELOCITY / 2
+			velocity.y = movement_data.jump_velocity
+	if not is_on_floor():
+		if Input.is_action_just_released("ui_accept") and velocity.y < movement_data.jump_velocity / 2:
+			velocity.y = movement_data.jump_velocity / 2
+			
+func apply_air_resistance(input_axis, delta):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistence * delta)
 
 func handle_acceleration(input_axis, delta):
 	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
 
 func apply_friction(input_axis, delta):
-	if input_axis == 0:
-		velocity.x = move_toward(velocity.x, 0, FRICTION  * delta)
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.friction  * delta)
