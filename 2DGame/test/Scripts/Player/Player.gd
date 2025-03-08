901c5c9a8847a4f19c36950e0a33287016b58102
TrollingboygamingYT <147536768+trollingboygamingYT@users.noreply.github.com>
2025-03-08 22:48:50 +0100

    Restart op alles

---
+++ 2DGame/test/Scripts/Player/Player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+const SPEED = 600.0
+const ACCELERATION = 600.0
+const FRICTION = 1000.0
+const JUMP_VELOCITY = -550.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	apply_gravity(delta)
+	handle_jump()
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	move_and_slide()
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
+
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+
+func apply_friction(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward(velocity.x, 0, FRICTION  * delta)
